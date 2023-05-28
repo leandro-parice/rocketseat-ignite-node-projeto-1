b4a192a5864c60c0e6bddc90126490916105f556
Leandro Parice <contato@leandroparice.com.br>
2023-05-28 10:58:12 -0300

    finalizado

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import http from "node:http";
 import { json } from "./middleware/json.js";
 import { routes } from "./routes.js";
+import { extractQueryParams } from "./utils/extract-query-params.js";
 
 const app = http.createServer(async (req, res) => {
   const { method, url } = req;
@@ -13,7 +14,12 @@ const app = http.createServer(async (req, res) => {
 
   if (route) {
     const routeParams = req.url.match(route.path);
-    req.params = {...routeParams.groups};
+
+    const { query, ...params } = routeParams.groups;
+
+    req.params = params;
+    req.query = query ? extractQueryParams(query) : {};
+
     return route.handler(req, res);
   }
 
